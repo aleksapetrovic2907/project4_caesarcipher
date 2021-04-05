33bdbc7f80ae70cd5faab3ce089bc11f7942fe6b
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-04-05 20:41:41 +0200

    Updated

--- caesar.rb
+++ caesar.rb
@@ -1,23 +1,36 @@
 # Caesars Cipher
 # crypt a string by shifting letters in message by 'X' in alphabet
 class String
-    def alpha?
-        self.between?('a', 'z') || self.between?('A', 'Z')
-    end
-end
-
-def caesar_cipher(message, shift)
-    crypted_message = ""
     
-    message.each_char do |letter|
-        ascii_offset = letter.ord < 91 ? 'A'.ord : 'a'.ord
+end
+
+class CaesarCipher
+    def initialize(message)
+        @message = message
+    end
+    attr_accessor :message
+
+    def encrypt(shift)
+        crypted_message = ""
         
-        if letter.alpha?
-            crypted_message += (((letter.ord - ascii_offset) + shift) % 26 + ascii_offset).chr
-        else
-            crypted_message += letter
+        message.each_char do |letter|
+            ascii_offset = letter.ord < 91 ? 'A'.ord : 'a'.ord
+            
+            if alpha?(letter)
+                crypted_message += (((letter.ord - ascii_offset) + shift) % 26 + ascii_offset).chr
+            else
+                crypted_message += letter
+            end
         end
+        crypted_message
     end
 
-    puts crypted_message
+    private
+    def alpha?(c)
+        c.between?('a', 'z') || c.between?('A', 'Z')
+    end
+
 end
+
+inst = CaesarCipher.new('What a string!')
+puts inst.encrypt(5)
